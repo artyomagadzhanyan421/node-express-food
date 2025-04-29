925588588f4ec35dee2bc15afdb4e97a4667955a
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-04-29 19:49:41 +0500

    rename recipes collection

--- mongodb/models/User.js
+++ mongodb/models/User.js
@@ -4,9 +4,9 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
-    savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
-    likedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
-    dislikedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }]
+    savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'recipes' }],
+    likedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'recipes' }],
+    dislikedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'recipes' }]
 });
 
 module.exports = mongoose.model('users', userSchema);
